5dbcb6268f841490b90e62ee69183ecefea3d0f8
Marco Túlio Teixeira <tulioteixeira@google.com>
2018-11-07 11:29:47 -0200

    Adding Datastore and BigQuery Helpers.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const PubSubHelper = require('./common/helper/PubSubHelper');
+const DatastoreHelper = require('./common/helper/DatastoreHelper');
+const BigQueryHelper = require('./common/helper/BigQueryHelper');
 
 module.exports = {
   PubSubHelper,
+  DatastoreHelper,
+  BigQueryHelper,
 };
