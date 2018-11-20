eb16043fdae4202b6eb924348ee8b5353f51742e
Marco Túlio Teixeira <tulioteixeira@google.com>
2018-11-20 14:39:29 -0200

    Add string utils.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const PubSubHelper = require('./common/helper/PubSubHelper');
 const DatastoreHelper = require('./common/helper/DatastoreHelper');
 const BigQueryHelper = require('./common/helper/BigQueryHelper');
 const ErrorUtil = require('./common/util/ErrorUtil');
+const StringUtil = require('./common/util/StringUtil');
 
 module.exports = {
   PubSubHelper,
   DatastoreHelper,
   BigQueryHelper,
   ErrorUtil,
+  StringUtil,
 };
