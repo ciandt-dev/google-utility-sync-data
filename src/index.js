6e74f3a7d39995235bba704e26903da62dd5ab2d
Marco Túlio Teixeira <tulioteixeira@google.com>
2018-11-20 11:56:24 -0200

    Add error util.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const PubSubHelper = require('./common/helper/PubSubHelper');
 const DatastoreHelper = require('./common/helper/DatastoreHelper');
 const BigQueryHelper = require('./common/helper/BigQueryHelper');
+const ErrorUtil = require('./common/util/ErrorUtil');
 
 module.exports = {
   PubSubHelper,
   DatastoreHelper,
   BigQueryHelper,
+  ErrorUtil,
 };
